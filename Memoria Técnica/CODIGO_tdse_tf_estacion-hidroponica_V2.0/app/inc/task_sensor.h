a513f7bf4bdad86bc5b903cbe91d767c006c610f
franavin <avincetto.f@gmail.com>
2026-08-10 12:49:41 -0300

    Nueva versión del código
    Subimos nueva versión del código que corrige varios errores respecto al anterior.

---
+++ Memoria Técnica/CODIGO_tdse_tf_estacion-hidroponica_V2.0/app/inc/task_sensor.h
@@ -0,0 +1,28 @@
+#ifndef TASK_SENSOR_H_
+#define TASK_SENSOR_H_
+
+/********************** CPP guard ********************************************/
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+/********************** inclusions *******************************************/
+
+/********************** macros ***********************************************/
+
+/********************** typedef **********************************************/
+
+/********************** external data declaration ****************************/
+
+/********************** external functions declaration ***********************/
+extern void task_sensor_init(void *parameters);
+extern void task_sensor_update(void *parameters);
+
+/********************** End of CPP guard *************************************/
+#ifdef __cplusplus
+}
+#endif
+
+#endif /* TASK_SENSOR_H_ */
+
+/********************** end of file ******************************************/
